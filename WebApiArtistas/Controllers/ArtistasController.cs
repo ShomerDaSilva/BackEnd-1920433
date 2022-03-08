5f5a74a062e2ba41985604c58470b7fd8e68db4e
Shomer Yail <shomer.dasilvarm@uanl.edu.mx>
2022-03-07 19:50:17 -0600

    act proyecto 28/2/22

--- WebApiArtistas/Controllers/ArtistasController.cs
+++ WebApiArtistas/Controllers/ArtistasController.cs
@@ -14,14 +14,85 @@ namespace WebApiArtistas.Controllers
             this.dbContext = context;
         }
 
-        [HttpGet]
+
+        [HttpGet] //api/artistas
+        [HttpGet("listado")] //api/artistas/listado
+        [HttpGet("/listado")] // /listado
         public async Task<ActionResult<List<Artista>>> Get()
         {
             return await dbContext.Artistas.Include(x => x.datos).ToListAsync();
         }
 
+
+
+        /* -----------------------------------------------------
+        * -----------------------------------------------------
+        *                     CLASE 21/02/21
+        * -----------------------------------------------------
+        * -----------------------------------------------------*/
+
+        [HttpGet("primero")] //api/artistas/primero
+        public async Task<ActionResult<Artista>> PrimerArtista([FromHeader] int valor, [FromQuery] string artista, [FromQuery] int artistaId)
+        {
+            return await dbContext.Artistas.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("primero2")] //api/artistas/primero
+        public ActionResult<Artista> PrimerArtistaD()
+        {
+            return new Artista() { Nombre = "Dos" };
+        }
+
+
+
+
+        /*[HttpGet("{id:int}")]
+         public async Task<ActionResult<Artista>> Get(int id)
+         {
+            var artista =  await dbContext.Artistas.FirstOrDefaultAsync(x => x.Id == id);
+
+             if(artista == null)
+             {
+                 return NotFound();
+             }
+             return artista;
+         }*/
+
+        //  CON STRING 
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Artista>> Get([FromRoute]string nombre)
+        {
+            var artista = await dbContext.Artistas.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+
+            if (artista == null)
+            {
+                return NotFound();
+            }
+            return artista;
+        }
+
+        // VAROPS PARAMETROS
+        [HttpGet("{id:int}/{param=Drake}")]
+        //[HttpGet("{id:int}/{param?}")]
+        public async Task<ActionResult<Artista>> Get(int id, string param)
+        {
+            var artista = await dbContext.Artistas.FirstOrDefaultAsync(x => x.Id == id);
+
+           if (artista == null)
+            {
+                return NotFound();
+            }
+            return artista;
+        }
+
+        /* -----------------------------------------------------
+        * -----------------------------------------------------
+        *                     CLASE 21/02/21
+        * -----------------------------------------------------
+        * -----------------------------------------------------*/
         [HttpPost]
-        public async Task<ActionResult> Post(Artista artista)
+        public async Task<ActionResult> Post([FromBody] Artista artista)
         {
             dbContext.Add(artista);
             await dbContext.SaveChangesAsync();
--- WebApiArtistas/Entidades/Artista.cs
+++ WebApiArtistas/Entidades/Artista.cs
@@ -1,10 +1,27 @@
-﻿namespace WebApiArtistas.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiArtistas.Entidades
 {
     public class Artista
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "el campo de nombre {0} es requerido")]
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} solo puede tener 10 caracteres")]
         public string Nombre { get; set; }
+
+        [Range(1, 100, ErrorMessage = "El campo Albums no se encuentra dentro del rango")]
+        [NotMapped]
+        public int Albums { get; set; }
+
+        [CreditCard]
+        [NotMapped]
+        public string Tarjeta { get; set; }
+
+        [Url]
+        [NotMapped]
+        public string Url { get; set; }
 
         public List<Dato> datos { get; set; }
     }
